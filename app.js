def887d97792fa397bfea0c9ccfab6c200f4b857
umidjonibrokihmovv <umidjonibrokhimov8@gmail.com>
2021-09-20 12:23:29 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log("Assalomu alaykom");
+console.log("Valeykom ASsalom")
+
+var person="23";
+console.log(person)
+
+var number1="23";
+var number2=43;
+var per=number1+number2;
+console.log(per)
+
+document.getElementById("men").innerHTML="Assalom Alaykom"
+
+// function myfunction() {
+//     var nam=33;
+//     console.log("myfunction");
+// }
+
+prompt("MENI BOS");
+
+var carname ="volvo"
+document.getElementById("sen").innerHTML=carname;
